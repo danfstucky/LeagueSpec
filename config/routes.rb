f538c904c1efdb6fa95b128836a322f74d07b938
Dan Stucky <danfstucky@gmail.com>
2016-01-11 19:12:34 -0600

    Login Page layout and Bootstrap integration

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
+  root                  'sessions#new'
+  post    'login'   =>  'sessions#create'
+  delete  'logout'  =>  'sessions#destroy'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
