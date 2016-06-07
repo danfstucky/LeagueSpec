47cae90bcf3afac96f54851ddac0a78518a5d34b
Sulai <seun.lasisi@yahoo.com>
2016-06-07 21:58:28 +0000

    Reduced database calls, removed return from functions, removed my api key. If error arises due to api key, look on slack and correct the error.

--- app/models/summoner.rb
+++ app/models/summoner.rb
@@ -1,43 +1,51 @@
-    require 'lol'
-    class Summoner < ActiveRecord::Base
-        
-        @@Summoner = Lol::SummonerRequest.new "833becde-d428-4d74-bd27-8418e1c0a33d", "na"
-        
-        attr_accessor :summoner_name
-       
-        def initialize(name)
-            @summoner_name=name ||= 'ahri'
+        require 'lol'
+        class Summoner < ActiveRecord::Base
+            
+            
+            
+            attr_accessor :summoner_name, :summoner
+           
+            def initialize(name)
+                @summonerReq = Lol::SummonerRequest.new "833becde-d428-4d74-bd27-8418e1c0a33d", "na"
+                @summoner_name=name ||= 'ahri'
+                @summonerObj = getSummonerObject
+            end
+            
+            def getSummonerObject
+                
+               return @summonerReq.by_name(summoner_name).first
+                
+            end
+            
+            def getSummonerId
+                
+               @summonerObj.id
+                
+            end
+            
+            
+            def getSummonerProfileIconId
+                
+                @summonerObj.profile_icon_id
+                
+            end
+           
+           def getSummonerLevel
+                
+                @summonerObj.summoner_level
+                
+           end
+           
+           def getSummonerRevisionDate
+                
+                @summonerObjrevision_date
+                
+           end
+            
+            def getSummonerRevisionDateString
+                
+                @summonerObj.first.revision_date_str
+                
+            end
+           
         end
-        
-        def getSummonerId
-            
-           return @@Summoner.by_name(summoner_name).first.id
-            
-        end
-        
-        
-        def getSummonerProfileIconId
-            
-           return @@Summoner.by_name(summoner_name).first.profile_icon_id
-            
-        end
-       
-       def getSummonerLevel
-            
-           return @@Summoner.by_name(summoner_name).first.summoner_level
-            
-       end
-       
-       def getSummonerRevisionDate
-            
-           return @@Summoner.by_name(summoner_name).first.revision_date
-            
-       end
-        
-        def getSummonerRevisionDateString
-            
-           return @@Summoner.by_name(summoner_name).first.revision_date_str
-            
-        end
-       
-    end
