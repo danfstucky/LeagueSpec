d6861f4b96e7fc8deb641cb06d74ee366938f56d
Dan Stucky <danfstucky@gmail.com>
2016-06-19 12:59:32 -0500

    Update host name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'ancient-island-99949.herokuapp.com'
+  host = 'league-spec.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address => 'smtp.sendgrid.net',
