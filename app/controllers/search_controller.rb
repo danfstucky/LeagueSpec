f1db4c232e86e138e1fec0667d2aa179ae6b1feb
Sulai <seun.lasisi@yahoo.com>
2016-07-22 23:39:26 -0400

    Validate summoner search Validate summoner search and fix floating number typos in show/search page.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,17 +1,16 @@
 class SearchController < ApplicationController
   include LolConnections
-  before_action :search_summoner, only: [:search]
-  
   def search
-    @name = params[:summoner]
-    if @name.to_s.length<=1
-      flash[:danger] = 'Invalid summoner name length. Verify valid summoner name and try again'
-      return
+    @summonerErrors = []
+    @summonerErrors << "Summoner name must be at least 2 characters, verify summoner name is valid and search again." if (params[:summoner].length < 2)
+    @summonerErrors << "Summoner name must be less than 24 characters, verify summoner name is valid and search again." if (params[:summoner].length > 24)
+    if @summonerErrors.empty?
+      @name = params[:summoner]
+      search_summoner
     else 
-      if params[:summoner]
-        search_summoner
-      end
+      redirect_to :back, alert: @summonerErrors[0]
     end
+    
   end
-  
+
 end
