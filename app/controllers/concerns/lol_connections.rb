fbac0e79ccc00eccefa89690890432c9cffc4783
Dan Stucky <danfstucky@gmail.com>
2016-07-27 20:05:30 -0500

    Clean and optimize profiles view and CSS.

--- app/controllers/concerns/lol_connections.rb
+++ app/controllers/concerns/lol_connections.rb
@@ -2,15 +2,19 @@ require 'lol'
 require 'certified'
 module LolConnections
   attr_accessor :summonerObj, :statsObj, :champsReq, :summonerReq, :statsReq
+
   def create_summoner_connection
     @summonerReq = Lol::SummonerRequest.new Rails.application.secrets.sulai_api_key, "na"
   end
+
   def create_stats_connection
     @statsReq = Lol::StatsRequest.new Rails.application.secrets.sulai_api_key, "na"
   end
+
   def create_champ_connection
     @champsReq = Lol::StaticRequest.new Rails.application.secrets.sulai_api_key, "na"
   end
+
    def create_search_connection
     create_summoner_connection
     create_champ_connection
@@ -40,40 +44,42 @@ module LolConnections
     @mostPlayedChampObj = @mostPlayedChampObj.paginate(:page => params[:page], :per_page => 5)
   end
   
-    def get_highest_KDR_champ
-    @highestKillDeathRatioChampObj = @statsObj.champions.sort_by{|x|  if x.stats.total_deaths_per_session >  0 then 
-                                                                        -((x.stats.most_champion_kills_per_session.to_f)/(x.stats.total_deaths_per_session))
-                                                                      else 
-                                                                        -(x.stats.most_champion_kills_per_session.to_f) 
-                                                                      end
+  def get_highest_KDR_champ
+    @highestKillDeathRatioChampObj = @statsObj.champions.sort_by{|x|  
+      if x.stats.total_deaths_per_session >  0 then 
+        -((x.stats.most_champion_kills_per_session.to_f)/(x.stats.total_deaths_per_session))
+      else 
+        -(x.stats.most_champion_kills_per_session.to_f) 
+      end
     }
     @highestKillDeathRatioChampObj = @highestKillDeathRatioChampObj.paginate(:page => params[:page], :per_page => 5)                                      
-    end
+  end
     
-    def get_highest_WLR_champ
+  def get_highest_WLR_champ
     #Same logic as above
     
-    @highestWinLossRatioChampObj = @statsObj.champions.sort_by{|x|  if x.stats.total_sessions_lost >  0 then 
-                                                                      -((x.stats.total_sessions_won.to_f)/(x.stats.total_sessions_lost))
-                                                                    else 
-                                                                      -(x.stats.total_sessions_won.to_f) 
-                                                                    end
+    @highestWinLossRatioChampObj = @statsObj.champions.sort_by{|x|  
+      if x.stats.total_sessions_lost >  0 then 
+        -((x.stats.total_sessions_won.to_f)/(x.stats.total_sessions_lost))
+      else 
+        -(x.stats.total_sessions_won.to_f) 
+      end
     }
     @highestWinLossRatioChampObj = @highestWinLossRatioChampObj.paginate(:page => params[:page], :per_page => 5)
+  end
+  def get_overall_WLR
+    overallLosses = 0
+    overallWins = 0                                              
+    @statsObj.champions.each do |x|
+      overallLosses += x.stats.total_sessions_lost
+      overallWins += x.stats.total_sessions_won 
     end
-    def get_overall_WLR
-        overallLosses = 0
-        overallWins = 0                                              
-        @statsObj.champions.each do |x|
-          overallLosses += x.stats.total_sessions_lost
-          overallWins += x.stats.total_sessions_won 
-        end
-        if overallLosses > 0 then                       
-          @overallWinLossRatio = (overallWins.to_f/overallLosses).round(2)
-        else
-          @overallWinLossRatio = overallWins.to_f
-        end
+    if overallLosses > 0 then                       
+      @overallWinLossRatio = (overallWins.to_f/overallLosses).round(2)
+    else
+      @overallWinLossRatio = overallWins.to_f
     end
+  end
 
   def get_overall_KDR
     overallKills = 0
@@ -90,32 +96,32 @@ module LolConnections
     
   end
   
-    def verify_summoner_name_and_stats
-#So far, only three requests are being used. Stats Request, Static Request and Summoner Request.
-#Static request doesn't blow up because it is static data that does not change .
-#Stats request and summoner request have blown up so far, so this verification prevents us from creating an account for someone
-#whose stats or summoner request will blow up.
+  def verify_summoner_name_and_stats
+    #So far, only three requests are being used. Stats Request, Static Request and Summoner Request.
+    #Static request doesn't blow up because it is static data that does not change .
+    #Stats request and summoner request have blown up so far, so this verification prevents us from creating an account for someone
+    #whose stats or summoner request will blow up.
 
-        begin
-          create_summoner_connection
-          @summonerObj = @summonerReq.by_name(user_params[:name]).first
-        rescue Lol::NotFound => e
-          if e.message == '404 Not Found'
-            flash[:danger] = "Summoner name has to be registered. Register on the LoL website and return here to sign up"
-            redirect_to new_profile_path
-            return
-          end
-        end
-        begin
-          create_stats_connection
-          @statsObj = statsReq.ranked(@summonerObj.id, extra = {})
-        rescue Lol::NotFound => e
-          if e.message == '404 Not Found'
-            flash[:danger] = "Summoner stats were not found. Verify that summoner has been active in the last calendar year and try signing up again."
-            redirect_to new_profile_path
-          end
-        end
+    begin
+      create_summoner_connection
+      @summonerObj = @summonerReq.by_name(user_params[:name]).first
+    rescue Lol::NotFound => e
+      if e.message == '404 Not Found'
+        flash[:danger] = "Summoner name has to be registered. Register on the LoL website and return here to sign up"
+        redirect_to new_profile_path
+        return
+      end
     end
+    begin
+      create_stats_connection
+      @statsObj = statsReq.ranked(@summonerObj.id, extra = {})
+    rescue Lol::NotFound => e
+      if e.message == '404 Not Found'
+        flash[:danger] = "Summoner stats were not found. Verify that summoner has been active in the last calendar year and try signing up again."
+        redirect_to new_profile_path
+      end
+    end
+  end
 
   def get_summoner
     create_summoner_connection
